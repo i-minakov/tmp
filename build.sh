4e9e9048bc0a7d44af3f6814bc61d356682c9507
iminakov <55349870+i-minakov@users.noreply.github.com>
2020-09-29 15:57:18 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-export PKG_CONFIG_PATH="-L~/.brew/Cellar/qt/5.15.1/lib/pkgconfig"
-export CMAKE_PREFIX_PATH="~/.brew/Cellar/qt/5.15.1//lib/cmake:$CMAKE_PREFIX_PATH"
+#!/bin/zsh
+
 cmake . -Bbuild -Wdev -Werror=dev && cmake --build ./build
 
 touch utag.sh
@@ -7,7 +7,9 @@ touch utag.sh
 echo "declare -i positon=0" >> utag.sh
 echo "for i in \$@" >> utag.sh
 echo "do" >> utag.sh
-echo "set -- \${@:1:\$positon} "\${PWD}/\$i" \${@:\$positon + 2}" >> utag.sh
+echo "if [[ \$i != /* ]];" >> utag.sh
+echo "then set -- \${@:1:\$positon} "\${PWD}/\$i" \${@:\$positon + 2};" >> utag.sh
+echo "fi" >> utag.sh
 echo "positon+=1" >> utag.sh
 echo "done" >> utag.sh
 echo "cd ${PWD}/build/app" >> utag.sh
